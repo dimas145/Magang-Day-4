d76e4fc33e11a7a69fcc39b56f3692da18cb6d48
modular-worm <59914510+modular-worm@users.noreply.github.com>
2020-03-07 15:59:07 +0700

    tambah nama

--- Hello.cpp
+++ Hello.cpp
@@ -3,8 +3,12 @@
 using namespace std;
 
 int main(){
-
-    cout << "Hello World by Erick\n";
-
+    cout << "Hello World\n";
+    cout << "Hello Dari Faishal\n";
+    cout << "Hello World dari Reyhan\n";
+    cout << "Hello World dari dimas\n";
+    cout << "Hello World dari emyr\n";
+    cout << "Hello World dari roman\n";
+    cout << "Hello World dari Erick\n";
     return 0;
 }
